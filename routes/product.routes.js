92ce4dfd35c3d3e9a8f00f03b251898de2ed520a
lontel <lonte.leon@gmail.com>
2023-01-26 09:24:14 +0100

    implement express-validator

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const productsController = require('../controllers/product.controller')
 const auth = require('../middleware/auth')
+const { addProductValidator } = require('../middleware/validations')
 
-router.post('/', auth('createAny', 'product'), productsController.addProduct)
+router.post('/', auth('createAny', 'product'), addProductValidator, productsController.addProduct)
 
 module.exports = router
