acda47d73b7fcd61fffe8ed6337c8eb03e294bf0
lontel <lonte.leon@gmail.com>
2023-01-26 07:20:14 +0100

    add create product route

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,14 @@ const { productService } = require('../services')
 
 
 const productsController = {
-   
+    async addProduct(req, res, next) {
+        try {
+            const product = await productService.addProduct(req.body)
+            res.json(product)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports = productsController
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const productsController = require('../controllers/product.controller')
+const auth = require('../middleware/auth')
 
-
+router.post('/', auth('createAny', 'product'), productsController.addProduct)
 
 module.exports = router
